8bf26991a31cd5ec064d0e88fd0354c9337827dc
Reem Krauss <reem.krauss@gmail.com>
2022-05-27 01:03:02 +0300

    day 26/5 end

--- src/cmps/song-preview.jsx
+++ src/cmps/song-preview.jsx
@@ -1,24 +1,25 @@
+import { Link } from "react-router-dom"
 import { useDispatch } from "react-redux"
 import { setMiniPlaylist } from "../store/actions/audio-player.action"
 import { BsPlayFill } from 'react-icons/bs'
 
 
 
-export const SongPreview = ({ song, playlistId }) => {
+export const SongPreview = ({ song, playlistId, isFromPlayer, playlistName }) => {
     const dispatch = useDispatch()
-    const { idx } = song
 
     const onSetMiniPlaylist = () => {
-        dispatch(setMiniPlaylist(playlistId, idx))
+        dispatch(setMiniPlaylist(playlistId, song.idx))
     }
 
 
     return <section className="song-preview flex">
         <div className="flex">
-            <h5>{idx + 1}</h5>
+            {!isFromPlayer && <h5>{song.idx + 1}</h5>}
             <img src={song.imgUrl} />
             <h4 className='song-title'>{song.title}</h4>
-            <BsPlayFill className="play-btn" onClick={onSetMiniPlaylist} />
+            {isFromPlayer && <Link to={`/playlist/${playlistId}`}>{playlistName}</Link>}
+            {!isFromPlayer && <BsPlayFill className="play-btn" onClick={onSetMiniPlaylist} />}
         </div>
     </section>
 }

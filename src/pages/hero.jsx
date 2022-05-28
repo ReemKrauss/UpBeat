8a3414c489e5ecea0cb38f4f17e7e32545269af4
idob <idobakimer@gmail.com>
2022-05-28 17:18:49 +0300

    hero now works on initial

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -1,26 +1,34 @@
+import { sessionService } from '../services/session.service';
+import { useState } from 'react';
+
+
 export const Hero = (props) => {
+
+
+
     return <section className="hero">
-        <div class="hero-txt-container">
-        <img src="../assets/upbeatlogograd.png" alt="" />
-        <h1>UpBeat</h1>
-        <h3>Feel the beat</h3>
-        <p>Here in UpBeat, we believe any song should be just a click away.</p>
-        <button class="hero-btn">Start listening</button>
+        <div className="hero-txt-container">
+            <img src="../assets/upbeatlogograd.png" alt="" />
+            <h1>UpBeat</h1>
+            <h3>Feel the beat</h3>
+            <p>Here in UpBeat, we believe any song should be just a click away.</p>
+            <button onClick={props.onInitialEntry} >Start Listening</button>
+
         </div>
 
-        <div class="hero-container">
-    <ul class="bg-bubbles">
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
-	</div>
+        <div className="hero-container">
+            <ul className="bg-bubbles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
     </section>
 }

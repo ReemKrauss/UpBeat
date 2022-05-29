c6ec7eef6a0a2523b3711459ae92a6d7cde78617
Reem Krauss <reem.krauss@gmail.com>
2022-05-29 22:38:41 +0300

    day 29/5 fix svgs

--- src/cmps/audio-player.jsx
+++ src/cmps/audio-player.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'
 import YouTube from 'react-youtube';
 
-import { FaPlay, FaPause, FaForward, FaBackward, FaVolumeUp, FaVolumeMute,FaHeart } from 'react-icons/fa'
+import { FaPlay, FaPause, FaForward, FaBackward, FaVolumeUp, FaVolumeMute, FaHeart } from 'react-icons/fa'
 import { FiRepeat } from 'react-icons/fi'
 import { TiArrowShuffle } from 'react-icons/ti'
 
@@ -133,7 +133,7 @@ class _AudioPlayer extends React.Component {
                             <span className='playlist-name'>{playlistName}</span>
                         </div>
                     </Link>
-                    <FaHeart className='like-btn'/>
+                    <FaHeart className='like-btn' />
                 </section>
 
                 <section className='player-controlers'>
@@ -141,11 +141,11 @@ class _AudioPlayer extends React.Component {
                         <TiArrowShuffle className={`shuffle btn ${props.isShuffled}`} onClick={props.toggleShuffle} />
                         <FaBackward className="change-song btn" onClick={this.onBackward} />
                         <button className="play btn" onClick={this.onTogglePlay}>
-                            {!props.isPlaying && <FaPlay className='play-svg' />}
-                            {props.isPlaying && <FaPause className='pause-svg' />}
+                            {!props.isPlaying && <svg role="img" height="16" width="16" className='play-svg' viewBox="0 0 16 16" ><path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path></svg>}
+                            {props.isPlaying && <svg role="img" height="16" width="16" className='pause-svg' viewBox="0 0 16 16" ><path d="M2.7 1a.7.7 0 00-.7.7v12.6a.7.7 0 00.7.7h2.6a.7.7 0 00.7-.7V1.7a.7.7 0 00-.7-.7H2.7zm8 0a.7.7 0 00-.7.7v12.6a.7.7 0 00.7.7h2.6a.7.7 0 00.7-.7V1.7a.7.7 0 00-.7-.7h-2.6z"></path></svg>}
                         </button>
                         <FaForward className="change-song btn" onClick={this.onForward} />
-                        <FiRepeat className={`repeat btn ${state.isRepeat}`} onClick={this.onToggleRepeat} />
+                        <svg role="img" height="16" width="16" viewBox="0 0 16 16" className={`repeat btn ${state.isRepeat}`} onClick={this.onToggleRepeat} ><path d="M0 4.75A3.75 3.75 0 013.75 1h8.5A3.75 3.75 0 0116 4.75v5a3.75 3.75 0 01-3.75 3.75H9.81l1.018 1.018a.75.75 0 11-1.06 1.06L6.939 12.75l2.829-2.828a.75.75 0 111.06 1.06L9.811 12h2.439a2.25 2.25 0 002.25-2.25v-5a2.25 2.25 0 00-2.25-2.25h-8.5A2.25 2.25 0 001.5 4.75v5A2.25 2.25 0 003.75 12H5v1.5H3.75A3.75 3.75 0 010 9.75v-5z"></path></svg>
                     </div>
                     <div className="time-container flex">
                         <span>{this.currTimePassStr}</span>
@@ -157,8 +157,8 @@ class _AudioPlayer extends React.Component {
                     </div>
                 </section>
                 <div className='volume flex'>
-                    {!state.isMute && <FaVolumeUp className="volume-btn btn" onClick={this.onToggleMute} />}
-                    {state.isMute && <FaVolumeMute className="volume-btn btn" onClick={this.onToggleMute} />}
+                    {state.isMute && <svg role="presentation" height="16" width="16" className="volume-btn btn" onClick={this.onToggleMute} aria-label="Volume off" id="volume-icon" viewBox="0 0 16 16" ><path d="M13.86 5.47a.75.75 0 00-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 008.8 6.53L10.269 8l-1.47 1.47a.75.75 0 101.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 001.06-1.06L12.39 8l1.47-1.47a.75.75 0 000-1.06z"></path><path d="M10.116 1.5A.75.75 0 008.991.85l-6.925 4a3.642 3.642 0 00-1.33 4.967 3.639 3.639 0 001.33 1.332l6.925 4a.75.75 0 001.125-.649v-1.906a4.73 4.73 0 01-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 01-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path></svg>}
+                    {!state.isMute && <svg role="presentation" width="16" height="16" className='volume-btn btn' onClick={this.onToggleMute} aria-label="Volume high" id="volume-icon" viewBox="0 0 16 16" ><path d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 010 4.88z"></path><path d="M11.5 13.614a5.752 5.752 0 000-11.228v1.55a4.252 4.252 0 010 8.127v1.55z"></path></svg>}
                     <div className='player-timer volume-control'>
                         <progress className='volume-progress' min="0" max="100" value={volume}></progress>
                         <input type="range" id="volume-slider" className="volume-slider" min="0" max="100" value={volume} onChange={this.onChangeVolume} />

abaf194452eaec9ec1ae34f51d013c86f6f835ca
Reem Krauss <reem.krauss@gmail.com>
2022-05-30 18:34:08 +0300

    day 30/5 plyer tuch

--- src/cmps/audio-player.jsx
+++ src/cmps/audio-player.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import YouTube from 'react-youtube';
 
 import { FaForward, FaBackward, FaHeart } from 'react-icons/fa'
+import pause from '../assets/img/pause.svg'
 
 
 import { setPlayer, togglePlay, changeSong, setCurrTimePass, toggleShuffle } from '../store/actions/audio-player.action';
@@ -158,17 +159,17 @@ class _AudioPlayer extends React.Component {
                         <span>{this.currTimePassStr}</span>
                         <div className='player-timer'>
                             <progress min="0" max={song.duration.total} value={this.props.currTimePass}></progress>
-                            <input type="range" id="duration" className="duration" min="0" max={song.duration.total} value={this.props.currTimePass} onChange={this.onChangeDuration} onMouseUp={(ev) => this.onChangeDuration(ev, true)} />
+                            <input type="range" id="duration" className="duration" min="0" max={song.duration.total} value={this.props.currTimePass} onChange={this.onChangeDuration} onTouchEnd={(ev) => this.onChangeDuration(ev, true)} onMouseUp={(ev) => this.onChangeDuration(ev, true)} />
                         </div>
                         <span>{song.duration.display}</span>
                     </div>
                 </section>
                 <div className='volume flex'>
-                    {state.isMute && <svg role="presentation" height="16" width="16" className="volume-btn btn" onClick={this.onToggleMute} aria-label="Volume off" id="volume-icon" viewBox="0 0 16 16" ><path d="M13.86 5.47a.75.75 0 00-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 008.8 6.53L10.269 8l-1.47 1.47a.75.75 0 101.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 001.06-1.06L12.39 8l1.47-1.47a.75.75 0 000-1.06z"></path><path d="M10.116 1.5A.75.75 0 008.991.85l-6.925 4a3.642 3.642 0 00-1.33 4.967 3.639 3.639 0 001.33 1.332l6.925 4a.75.75 0 001.125-.649v-1.906a4.73 4.73 0 01-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 01-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path></svg>}
+                    {state.isMute && <svg role="presentation" height="16" width="16" className="volume-btn btn"  onClick={this.onToggleMute} aria-label="Volume off" id="volume-icon" viewBox="0 0 16 16" ><path d="M13.86 5.47a.75.75 0 00-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 008.8 6.53L10.269 8l-1.47 1.47a.75.75 0 101.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 001.06-1.06L12.39 8l1.47-1.47a.75.75 0 000-1.06z"></path><path d="M10.116 1.5A.75.75 0 008.991.85l-6.925 4a3.642 3.642 0 00-1.33 4.967 3.639 3.639 0 001.33 1.332l6.925 4a.75.75 0 001.125-.649v-1.906a4.73 4.73 0 01-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 01-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path></svg>}
                     {!state.isMute && <svg role="presentation" width="16" height="16" className='volume-btn btn' onClick={this.onToggleMute} aria-label="Volume high" id="volume-icon" viewBox="0 0 16 16" ><path d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 010 4.88z"></path><path d="M11.5 13.614a5.752 5.752 0 000-11.228v1.55a4.252 4.252 0 010 8.127v1.55z"></path></svg>}
                     <div className='player-timer volume-control'>
                         <progress className='volume-progress' min="0" max="100" value={volume}></progress>
-                        <input type="range" id="volume-slider" className="volume-slider" min="0" max="100" value={volume} onChange={this.onChangeVolume} />
+                        <input type="range" id="volume-slider" className="volume-slider" min="0" max="100" value={volume} onTouchEnd={this.onChangeVolume} onChange={this.onChangeVolume} />
                     </div>
                 </div>
             </div>

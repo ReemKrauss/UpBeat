317e4c94841b2d7d8f721c9bcca60ca5515a0dec
Reem Krauss <reem.krauss@gmail.com>
2022-06-01 19:51:25 +0300

    day 1/6 fix bug with player

--- src/cmps/audio-player.jsx
+++ src/cmps/audio-player.jsx
@@ -22,6 +22,10 @@ class _AudioPlayer extends React.Component {
     }
 
     timerIntervalId
+    intervalCb = () => {
+        const currTimePass = this.props.player.getCurrentTime()
+        this.props.setCurrTimePass(currTimePass)
+    }
 
     componentWillUnmount = () => {
         clearInterval(this.timerIntervalId)
@@ -31,7 +35,7 @@ class _AudioPlayer extends React.Component {
         this.props.setPlayer(ev.target)
         if (!this.props.isPlaying) this.props.togglePlay()
         else ev.target.playVideo()
-        if(this.state.isMute)ev.target.setVolume(0)
+        if (this.state.isMute) ev.target.setVolume(0)
         else ev.target.setVolume(this.state.volume)
     }
 
@@ -48,10 +52,7 @@ class _AudioPlayer extends React.Component {
         }
         if (ev.data === 1) {
             if (this.timerIntervalId) clearInterval(this.timerIntervalId)
-            this.timerIntervalId = setInterval(() => {
-                const currTimePass = this.props.player.getCurrentTime()
-                this.props.setCurrTimePass(currTimePass)
-            }, 1000)
+            this.timerIntervalId = setInterval(this.intervalCb, 1000)
         }
     }
 
@@ -103,8 +104,13 @@ class _AudioPlayer extends React.Component {
     }
 
     onChangeDuration = (ev, isTouchEnd) => {
+        if (this.timerIntervalId) clearInterval(this.timerIntervalId)
         this.props.setCurrTimePass(+ev.target.value)
-        if (isTouchEnd) this.props.player.seekTo(+ev.target.value)
+        if (isTouchEnd){
+            this.props.player.seekTo(+ev.target.value)
+            this.timerIntervalId = setInterval(this.intervalCb, 1000)
+        } 
+            
     }
 
     get currTimePassStr() {
@@ -136,13 +142,13 @@ class _AudioPlayer extends React.Component {
                         <img className='song-img' src={song.imgUrl} />
                         <div>
                             <div className='song-title'>
-                            <h4 className='song-txt'>{song.title}</h4>
+                                <h4 className='song-txt'>{song.title}</h4>
                             </div>
                             <span className='playlist-name'>{playlistName}</span>
                         </div>
                     </Link>
                     <FaHeart className='like-btn' />
-                    {props.isPlaying&&<img className={`audio-svg ${props.isPlaying}`} src={audioSvg} alt="err" />}
+                    {props.isPlaying && <img className={`audio-svg ${props.isPlaying}`} src={audioSvg} alt="err" />}
                 </section>
 
                 <section className='player-controlers'>
@@ -166,7 +172,7 @@ class _AudioPlayer extends React.Component {
                     </div>
                 </section>
                 <div className='volume flex'>
-                    {state.isMute && <svg role="presentation" height="16" width="16" className="volume-btn btn"  onClick={this.onToggleMute} aria-label="Volume off" id="volume-icon" viewBox="0 0 16 16" ><path d="M13.86 5.47a.75.75 0 00-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 008.8 6.53L10.269 8l-1.47 1.47a.75.75 0 101.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 001.06-1.06L12.39 8l1.47-1.47a.75.75 0 000-1.06z"></path><path d="M10.116 1.5A.75.75 0 008.991.85l-6.925 4a3.642 3.642 0 00-1.33 4.967 3.639 3.639 0 001.33 1.332l6.925 4a.75.75 0 001.125-.649v-1.906a4.73 4.73 0 01-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 01-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path></svg>}
+                    {state.isMute && <svg role="presentation" height="16" width="16" className="volume-btn btn" onClick={this.onToggleMute} aria-label="Volume off" id="volume-icon" viewBox="0 0 16 16" ><path d="M13.86 5.47a.75.75 0 00-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 008.8 6.53L10.269 8l-1.47 1.47a.75.75 0 101.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 001.06-1.06L12.39 8l1.47-1.47a.75.75 0 000-1.06z"></path><path d="M10.116 1.5A.75.75 0 008.991.85l-6.925 4a3.642 3.642 0 00-1.33 4.967 3.639 3.639 0 001.33 1.332l6.925 4a.75.75 0 001.125-.649v-1.906a4.73 4.73 0 01-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 01-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path></svg>}
                     {!state.isMute && <svg role="presentation" width="16" height="16" className='volume-btn btn' onClick={this.onToggleMute} aria-label="Volume high" id="volume-icon" viewBox="0 0 16 16" ><path d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 010 4.88z"></path><path d="M11.5 13.614a5.752 5.752 0 000-11.228v1.55a4.252 4.252 0 010 8.127v1.55z"></path></svg>}
                     <div className='player-timer volume-control'>
                         <progress className='volume-progress' min="0" max="100" value={volume}></progress>
